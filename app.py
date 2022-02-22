77dbff357be77442fa4e8e97cb7afc31dc9ef2d0
Toby Pickard <Tobias.Pickard-CIC-UK@ibm.com>
2022-02-22 16:52:31 +0000

    slowly building up a basic website

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 @app.route('/')
 def index():
