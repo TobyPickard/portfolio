89309da4ea1874d5cdd0a9540c01edbe6853c2c0
Toby Pickard <Tobias.Pickard-CIC-UK@ibm.com>
2022-02-25 17:56:51 +0000

    small changes to the the structure of the review.html and app.py files

--- app.py
+++ app.py
@@ -2,17 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-    
-@app.get('/shutdown')
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
-
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -56,4 +45,7 @@ def action():
             print("please try again")
             break
     
-    return render_template('review.html')
+    return render_template('review.html'), '400'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
