f8e610619e95c2f79744040a8332f5ffb6cc7146
Toby Pickard <thetobzz@yahoo.co.uk>
2023-11-25 13:31:24 +0000

    fixing another issue

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def about():
 def contact():
     return render_template('user/contact.html')
 
-@app.route('/admin_add_project')
-def admin_add_project():
-    return render_template('admin/add_project.html')
-
 @app.route('/projects')
 def projects():
     return render_template('user/projects.html')
+
+@app.route('/admin_add_project')
+def admin_add_project():
+    return render_template('admin/add_project.html')
 
 @app.route('/add_project', methods=['POST'])
 def add_project():
