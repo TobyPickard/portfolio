58e5f4f07329c4c1ebdc1099c5d2fd6486e73982
Toby Pickard <theobzz@yahoo.co.uk>
2023-11-28 11:30:52 +0000

    fixing edit data lints

--- Experiments/edit_data.py
+++ Experiments/edit_data.py
@@ -1,7 +1,22 @@
-import sqlite3 
-data = {'id': '1', 'name': '.vefve', 'purpose': '', 'objective': '', 'stack': '', 'start': '', 'end': '', 'github': '', 'result': 'boobies'}
+'''
+    This was just an experiment for editing project data.
+'''
 
-id = data.pop('id')
+import sqlite3
+
+data = {
+    'id': '1',
+    'name': '.vefve',
+    'purpose': '',
+    'objective': '',
+    'stack': '',
+    'start': '',
+    'end': '',
+    'github': '',
+    'result': 'boobies'
+}
+
+my_id = data.pop('id')
 
 connection = sqlite3.connect('example.db')
 cursor = connection.cursor()
@@ -9,7 +24,7 @@ cursor = connection.cursor()
 
 for k,v in data.items():
     query = f'''UPDATE projects SET {k}=? WHERE id=?'''
-    cursor.execute(query, (v, id))
+    cursor.execute(query, (v, my_id))
     print(query)
 
 connection.commit()
